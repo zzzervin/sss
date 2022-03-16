73d89944aeced798f090d9baa61f5bdc31470f8b
opilane <opilane@HP-CZC6297B8P>
2022-03-16 10:11:34 +0200

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uss_Mangi
+{
+    class Point
+    {
+
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point() { }
+
+        public Point(int _x, int _y, char _sym)//конструктор приравнивает _x,  _y,  _sym к   x, y, sym
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uss_Mangi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1,3,'*');// отправляют аргументы в конструктор Point в классе Point
+
+            p1.Draw();//Выводет данные из метода Draw в классе Point
+
+            Point p2 = new Point(4, 5, '*');
+            
+            p2.Draw();
+          
+            Console.ReadLine();
+        }
+
+      
+    }
+}
